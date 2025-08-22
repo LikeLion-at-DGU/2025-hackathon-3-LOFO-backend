80b33a37cd91a44b051446f042b30b89b2147034
lo2q <suhyeonkvvon@gmail.com>
2025-08-22 18:36:40 +0900

    fix: OutcomeFile 모델 형식 수정

--- outcomes/urls.py
+++ outcomes/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("mypage/portfolio", views.youth_portfolio, name="youth-portfolio"),
     path("mypage/insights", views.youth_insights, name="youth-insights"),
     path("mypage/saved", views.youth_saved, name="youth-saved"),
-    path("files/<int:file_id>/video-thumb", views.video_thumb, name="video-thumb"), #영상 썸네일
     path("feedback/<int:outcome_id>", views.outcome_feedback_detail, name="outcome-feedback-detail"), #피드백 확인
 ]
